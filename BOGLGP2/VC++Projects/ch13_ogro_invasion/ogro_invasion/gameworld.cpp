4af4297c82aa96d513e2488228e714ca4006eae2
Katianie9@gmail.com <Katianie9@gmail.com@c02fe8fd-aae5-f472-ec9b-07afcb1959a2>
2011-11-10 21:20:36 +0000

    Atleast im not crashing

--- BOGLGP2/VC++Projects/ch13_ogro_invasion/ogro_invasion/gameworld.cpp
+++ BOGLGP2/VC++Projects/ch13_ogro_invasion/ogro_invasion/gameworld.cpp
@@ -171,29 +171,30 @@ bool GameWorld::initialize()
 {
     srand((unsigned int)time(0));
 
+	spawnEntity(SKY);
+
     spawnEntity(LANDSCAPE); //Spawn the landscape
 
-	//spawnEntity(SKY);
+	
+  //  //Spawn a load of monsters
+  //  for (unsigned int i = 0; i < MAX_ENEMY_COUNT; ++i)
+  //  {
+  //      Entity* newEntity = spawnEntity(OGRO);
+  //      newEntity->setPosition(getRandomPosition());
+  //  }
 
-    //Spawn a load of monsters
-    for (unsigned int i = 0; i < MAX_ENEMY_COUNT; ++i)
-    {
-        Entity* newEntity = spawnEntity(OGRO);
-        newEntity->setPosition(getRandomPosition());
-    }
+  //  for (int i = 0; i < TREE_COUNT; ++i)
+  //  {
+  //      Entity* newEntity = spawnEntity(TREE);
 
-    for (int i = 0; i < TREE_COUNT; ++i)
-    {
-        Entity* newEntity = spawnEntity(TREE);
+  //      Vector3 pos(0.0f, -1.0f, 0.0f);
+  //      while (pos.y < 1.1f) 
+		//{
+  //          pos = getRandomPosition();
+  //      }
 
-        Vector3 pos(0.0f, -1.0f, 0.0f);
-        while (pos.y < 1.1f) 
-		{
-            pos = getRandomPosition();
-        }
-
-        newEntity->setPosition(pos);
-    }
+  //      newEntity->setPosition(pos);
+  //  }
 
     //Spawn the player and center them
     spawnEntity(PLAYER);
